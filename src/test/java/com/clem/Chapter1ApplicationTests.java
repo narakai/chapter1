b6b3d3f02d190c4064e3646e05c291bdf086fdf9
laileon <leon@igola.com>
2017-03-13 02:05:53 +0800

    #leon: user mybatis instead of jpa and jdbc

--- src/test/java/com/clem/Chapter1ApplicationTests.java
+++ src/test/java/com/clem/Chapter1ApplicationTests.java
@@ -1,9 +1,13 @@
 package com.clem;
 
 import com.clem.domain.User;
-import com.clem.domain.UserRepository;
+import com.clem.domain.UserMapper;
 import com.clem.web.HelloController;
 import com.clem.web.UserController;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -11,11 +15,14 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import java.io.Reader;
 
 import static org.hamcrest.Matchers.equalTo;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
@@ -28,97 +35,144 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class Chapter1ApplicationTests {
-	private MockMvc mvc;
+    private MockMvc mvc;
 
-	@Autowired
-	private UserRepository userRepository;
+//    @Autowired
+//    private UserRepository userRepository;
 
-	@Before
-	public void setUp() throws Exception {
-		mvc = MockMvcBuilders.standaloneSetup(new HelloController(), new UserController()).build();
-	}
-	@Test
-	public void getHello() throws Exception {
-		mvc.perform(MockMvcRequestBuilders.get("/hello").accept(MediaType.APPLICATION_JSON))
-				.andExpect(status().isOk())
-				.andExpect(content().string(equalTo("Hello World")));
-	}
+    @Before
+    public void setUp() throws Exception {
+        mvc = MockMvcBuilders.standaloneSetup(new HelloController(), new UserController()).build();
+    }
 
-	@Test
-	public void testUserController() throws Exception {
+    @Test
+    public void getHello() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.get("/hello").accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().string(equalTo("Hello World")));
+    }
+
+    @Test
+    public void testUserController() throws Exception {
 //  	测试UserController
-		RequestBuilder request = null;
+        RequestBuilder request = null;
 
-		// 1、get查一下user列表，应该为空
-		request = get("/users/");
-		mvc.perform(request)
-				.andExpect(status().isOk())
-				.andExpect(content().string(equalTo("[]")));
+        // 1、get查一下user列表，应该为空
+        request = get("/users/");
+        mvc.perform(request)
+                .andExpect(status().isOk())
+                .andExpect(content().string(equalTo("[]")));
 
-		// 2、post提交一个user
-		request = post("/users/")
-				.param("id", "1")
-				.param("name", "测试大师")
-				.param("age", "20");
-		mvc.perform(request)
+        // 2、post提交一个user
+        request = post("/users/")
+                .param("id", "1")
+                .param("name", "测试大师")
+                .param("age", "20");
+        mvc.perform(request)
 //				.andDo(MockMvcResultHandlers.print())
-				.andExpect(content().string(equalTo("success")));
+                .andExpect(content().string(equalTo("success")));
 
-		// 3、get获取user列表，应该有刚才插入的数据
-		request = get("/users/");
-		mvc.perform(request)
-				.andExpect(status().isOk())
-				.andExpect(content().string(equalTo("[{\"id\":1,\"name\":\"测试大师\",\"age\":20}]")));
+        // 3、get获取user列表，应该有刚才插入的数据
+//        request = get("/users/");
+//        mvc.perform(request)
+//                .andExpect(status().isOk())
+//                .andExpect(content().string(equalTo("[{\"id\":1,\"name\":\"测试大师\",\"age\":20}]")));
 
-		// 4、put修改id为1的user
-		request = put("/users/1")
-				.param("name", "测试终极大师")
-				.param("age", "30");
-		mvc.perform(request)
-				.andExpect(content().string(equalTo("success")));
+        // 4、put修改id为1的user
+        request = put("/users/1")
+                .param("name", "测试终极大师")
+                .param("age", "30");
+        mvc.perform(request)
+                .andExpect(content().string(equalTo("success")));
 
-		// 5、get一个id为1的user
-		request = get("/users/1");
-		mvc.perform(request)
-				.andExpect(content().string(equalTo("{\"id\":1,\"name\":\"测试终极大师\",\"age\":30}")));
+        // 5、get一个id为1的user
+//        request = get("/users/1");
+//        mvc.perform(request)
+//                .andExpect(content().string(equalTo("{\"id\":1,\"name\":\"测试终极大师\",\"age\":30}")));
 
-		// 6、del删除id为1的user
-		request = delete("/users/1");
-		mvc.perform(request)
-				.andExpect(content().string(equalTo("success")));
+        // 6、del删除id为1的user
+        request = delete("/users/1");
+        mvc.perform(request)
+                .andExpect(content().string(equalTo("success")));
 
-		// 7、get查一下user列表，应该为空
-		request = get("/users/");
-		mvc.perform(request)
-				.andExpect(status().isOk())
-				.andExpect(content().string(equalTo("[]")));
+        // 7、get查一下user列表，应该为空
+        request = get("/users/");
+        mvc.perform(request)
+                .andExpect(status().isOk())
+                .andExpect(content().string(equalTo("[]")));
 
-	}
+    }
 
-	@Test
-	public void test() throws Exception {
-		// 创建10条记录
-		userRepository.save(new User("AAA", 10));
-		userRepository.save(new User("BBB", 20));
-		userRepository.save(new User("CCC", 30));
-		userRepository.save(new User("DDD", 40));
-		userRepository.save(new User("EEE", 50));
-		userRepository.save(new User("FFF", 60));
-		userRepository.save(new User("GGG", 70));
-		userRepository.save(new User("HHH", 80));
-		userRepository.save(new User("III", 90));
-		userRepository.save(new User("JJJ", 100));
-		// 测试findAll, 查询所有记录
-		Assert.assertEquals(10, userRepository.findAll().size());
-		// 测试findByName, 查询姓名为FFF的User
-		Assert.assertEquals(60, userRepository.findByName("FFF").getAge().longValue());
-		// 测试findUser, 查询姓名为FFF的User
-		Assert.assertEquals(60, userRepository.findUser("FFF").getAge().longValue());
-		// 测试findByNameAndAge, 查询姓名为FFF并且年龄为60的User
-		Assert.assertEquals("FFF", userRepository.findByNameAndAge("FFF", 60).getName());
-		// 测试删除姓名为AAA的User
-		userRepository.delete(userRepository.findByName("AAA"));
-		// 测试findAll, 查询所有记录, 验证上面的删除是否成功
-		Assert.assertEquals(9, userRepository.findAll().size());
-	}
+    //jpa test
+//    @Test
+//    public void test() throws Exception {
+//        // 创建10条记录
+//        userRepository.deleteAll();
+//        userRepository.save(new User("AAA", 10));
+//        userRepository.save(new User("BBB", 20));
+//        userRepository.save(new User("CCC", 30));
+//        userRepository.save(new User("DDD", 40));
+//        userRepository.save(new User("EEE", 50));
+//        userRepository.save(new User("FFF", 60));
+//        userRepository.save(new User("GGG", 70));
+//        userRepository.save(new User("HHH", 80));
+//        userRepository.save(new User("III", 90));
+//        userRepository.save(new User("JJJ", 100));
+//        // 测试findAll, 查询所有记录
+//        Assert.assertEquals(10, userRepository.findAll().size());
+//        // 测试findByName, 查询姓名为FFF的User
+//        Assert.assertEquals(60, userRepository.findByName("FFF").getAge().longValue());
+//        // 测试findUser, 查询姓名为FFF的User
+//        Assert.assertEquals(60, userRepository.findUser("FFF").getAge().longValue());
+//        // 测试findByNameAndAge, 查询姓名为FFF并且年龄为60的User
+//        Assert.assertEquals("FFF", userRepository.findByNameAndAge("FFF", 60).getName());
+//        // 测试删除姓名为AAA的User
+//        userRepository.delete(userRepository.findByName("AAA"));
+//        // 测试findAll, 查询所有记录, 验证上面的删除是否成功
+//        Assert.assertEquals(9, userRepository.findAll().size());
+//    }
+
+//    @Autowired
+//    private UserMapper userMapper;
+//
+//    @Test
+//    @Rollback
+//    public void findByName() throws Exception {
+//        User u = userMapper.findByName("JJJ");
+//        Assert.assertEquals(100, u.getAge().intValue());
+//    }
+//
+//    @Test
+//    @Rollback
+//    public void testUserMapper() throws Exception {
+//        List<User> userList = userMapper.findAll();
+//        for(User user : userList) {
+//            Assert.assertEquals(null, user.getId());
+//            Assert.assertNotEquals(null, user.getName());
+//            Assert.assertNotEquals(null, user.getAge());
+//        }
+//    }
+
+
+    @Autowired
+    private UserMapper userMapper;
+
+    @Test
+    public void testMybatis() throws Exception {
+//        String resource = "mybatis_config.xml";
+//        Reader reader = Resources.getResourceAsReader(resource);
+//        SqlSessionFactory sqlMapper = new SqlSessionFactoryBuilder().build(reader);
+//        //need add mapper
+//        sqlMapper.getConfiguration().addMapper(UserMapper.class);
+//        SqlSession sqlSession = sqlMapper.openSession();
+
+        userMapper.deleteAll("Leon");
+        User user = new User();
+        user.setName("Leon");
+        user.setAge(32);
+//        userMapper = sqlSession.getMapper(UserMapper.class);
+        userMapper.insert(user);
+        Assert.assertEquals(32, userMapper.selectByName("Leon").getAge().longValue());
+    }
+
 }
